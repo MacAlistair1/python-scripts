061df8808773e6a76e4e6af8a4aa4e5bfa24b8a3
Jeeven Lamichhane <lamichhaneaj@gmail.com>
2023-05-10 21:11:07 +0545

    updates: shell scripts and log folders

--- run_oil.sh
+++ run_oil.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /home/superx/Documents/scripts
+export root=$HOME/Documents/scripts
+cd $root
 python3 oil.py
 cd output
 git checkout
-echo "Script ran at $(date)" >> /home/superx/Documents/scripts/run.log
-echo "Script has run $(wc -l < /home/superx/Documents/scripts/run.log) times" >> /home/superx/Documents/scripts/run.log
+echo "Script ran at $(date)" >> $root/logs/oil_"$(date '+%Y-%m-%d')".log
+echo "Script has run $(wc -l < $root/logs/oil_"$(date '+%Y-%m-%d')".log) times" >> $root/logs/oil_"$(date '+%Y-%m-%d')".log
---
+++ run_share_market.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+export root=$HOME/Documents/scripts
+
+# Set the path to your share_market.py script
+SHARE_MARKET_SCRIPT=$root"/share_market.py"
+
+OUTPUT_FOLDER=$root"/output"
+
+# Define the start and end times (in 24-hour format)
+START_TIME="11:00"
+END_TIME="21:06"
+
+# Calculate the duration between the start and end times (in seconds)
+START_SECONDS=$(date -d "$START_TIME" +%s)
+END_SECONDS=$(date -d "$END_TIME" +%s)
+DURATION=$((END_SECONDS - START_SECONDS))
+
+# Run the script at 2-minute intervals within the specified time range
+while true; do
+    CURRENT_TIME=$(date +"%T")
+    CURRENT_SECONDS=$(date -d "$CURRENT_TIME" +%s)
+
+    if ((CURRENT_SECONDS >= START_SECONDS && CURRENT_SECONDS <= END_SECONDS)); then
+        python3 $SHARE_MARKET_SCRIPT
+
+        # git checkout to the output folder
+        git -C "$OUTPUT_FOLDER" checkout
+
+        echo "Script ran at $(date)" >> $root/logs/share_market_"$(date '+%Y-%m-%d')".log
+        echo "Script has run $(wc -l < $root/logs/share_market_"$(date '+%Y-%m-%d')".log) times" >> $root/logs/share_market_"$(date '+%Y-%m-%d')".log
+
+    fi
+
+    if ((CURRENT_SECONDS >= END_SECONDS)); then
+        break
+    fi
+
+    sleep 120  # Sleep for 2 minutes before the next iteration
+done
