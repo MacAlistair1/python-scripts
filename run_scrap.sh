f252c375797a8f468b604ac9385c2648c3d12736
Jeeven Lamichanne <lamichhaneaj@gmail.com>
2025-04-22 16:34:16 +0545

    run scrap sh file update for rashifal

--- run_scrap.sh
+++ run_scrap.sh
@@ -12,7 +12,7 @@ log_file="$folder/scrap.log"
 cd $root
 source ~/myenv/bin/activate
 echo "Scrap started at $(date)" >> "$log_file"
-python bank_rate.py; python currency.py; python gold-silver.py;  python oil.py; python earthquake.py;  python share_market.py; python top_music_video.py; python top_singer.py; python patro.py; python channel_info.py; python latest_youtube_video.py; python latest_reel.py;  python earthquake.py;
+python bank_rate.py; python currency.py; python gold-silver.py;  python oil.py; python earthquake.py;  python share_market.py; python top_music_video.py; python top_singer.py; python patro.py; python channel_info.py; python latest_youtube_video.py; python latest_reel.py;  python earthquake.py; python rashifal.py;
 cd output
 git checkout
 printf "Scrap ended at $(date) \n\n" >> "$log_file"
